6b4194bfadf6395be50b6f2333651e1b6c50a73d
DevSecOpsEasy <146831240+DevSecOpsEasy@users.noreply.github.com>
2024-10-16 07:09:03 +0530

    Delete EKS-TF directory

--- EKS-TF/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "YOUR S3 NAME HERE IN quotations ONLY" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
-  }
-}
